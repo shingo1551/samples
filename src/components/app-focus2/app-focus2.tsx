c846d945d8c2e52e087c27245d8cbb92d0dc682a
shingo1551 <tanikawa@proub.net>
2021-08-20 22:02:34 +0900

    tabindex & disabled

--- src/components/app-focus2/app-focus2.tsx
+++ src/components/app-focus2/app-focus2.tsx
@@ -6,13 +6,31 @@ import { Component, h } from '@stencil/core';
   shadow: false,
 })
 export class AppFocus2 {
+  inputs = [] as HTMLInputElement[];
+  button = null as HTMLButtonElement;
 
   componentDidRender() {
-    document.querySelectorAll('input').forEach(elm => elm.addEventListener('keyup', this.focus));
+    this.inputs = [];
+    const div = document.querySelector('.app-focus2');
+    this.button = div.querySelector('button');
+
+    div.querySelectorAll('input').forEach(el => this.inputs.push(el));
+    this.inputs.sort((i1, i2) => i1.tabIndex - i2.tabIndex);
+
+    this.inputs.forEach((el) => {
+      el.addEventListener('keyup', (ev) => this.focus(ev));
+    })
   }
 
   focus = (ev: KeyboardEvent) => {
-    console.log(ev);
+    if (ev.key === 'Enter') {
+      const index = (ev.target as HTMLElement).tabIndex;
+      const next = this.inputs.find(el => index < el.tabIndex && !el.disabled);
+      if (next)
+        next.focus();
+      else
+        this.button.focus();
+    }
   }
 
   render() {
@@ -20,19 +38,19 @@ export class AppFocus2 {
       <div class="app-focus2">
         <p>FOCUS2</p>
         <div>
-          <input type="text" />
+          <input type="text" tabindex="4" />
         </div>
 
         <div>
-          <input type="text" />
+          <input type="text" tabindex="3" disabled />
         </div>
 
         <div>
-          <input type="text" />
+          <input type="text" tabindex="2" />
         </div>
 
         <div>
-          <input type="text" />
+          <input type="text" tabindex="1" />
         </div>
 
         <div>
