d202d7cd620673ef92bcede9962a305008294ffe
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2021-09-04 04:27:47 +0900

    col

--- src/components/app-table/app-table.tsx
+++ src/components/app-table/app-table.tsx
@@ -11,7 +11,21 @@ export class AppTable {
     return (
       <div class="app-table" >
         <p>TABLE</p>
-        <div>
+        <div class="table">
+          <div class="row">
+            <div class="col1">COL1</div>
+            <div class="col2">COL2</div>
+            <div class="col3">
+              <div class="left border">-</div>
+              <div class="center">99</div>
+              <div class="right border">+</div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col1">COL1</div>
+            <div class="col2">COL2</div>
+            <div class="col3">COL3</div>
+          </div>
         </div>
       </div>
     );
